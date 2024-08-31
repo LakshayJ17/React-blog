c3dbab3c2b7de7e791b99fbb9980966b7e632103
lakshay17 <lakshayjain01lj@gmail.com>
2024-08-31 21:50:18 +0530

    Authorisation

--- BlogProject/src/appwrite/auth.js
+++ BlogProject/src/appwrite/auth.js
@@ -17,7 +17,6 @@ export class AuthService {
             const userAccount = await this.account.create(ID.unique(),email, password,name);
 
             if (userAccount){
-                // call another method
                 return this.login({email,password})
             } else{
                 return userAccount;
