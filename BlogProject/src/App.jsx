af0ef7af77522905bc9a4cdbf68d874d43238865
lakshay17 <lakshayjain01lj@gmail.com>
2024-09-03 21:27:24 +0530

    updated

--- BlogProject/src/App.jsx
+++ BlogProject/src/App.jsx
@@ -21,6 +21,8 @@ function App() {
       .finally(() => setLoading(false))
   }, [])
 
+  
+
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
